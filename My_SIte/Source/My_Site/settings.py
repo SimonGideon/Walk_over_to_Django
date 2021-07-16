125bdbaeecb4f6b105c1b5b0caba2b84f630564a
SimonGideon <simongideon918@gmail.com>
2021-07-16 19:39:28 +0300

    templating

--- My_SIte/Source/My_Site/settings.py
+++ My_SIte/Source/My_Site/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-1wp_37&b=oa12&4-h^$bd+_#*8rc8(ym$!by%)nrs6c_eof#df
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'My_Site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'My_Site.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- My_SIte/Source/news/views.py
+++ My_SIte/Source/news/views.py
@@ -1,13 +1,9 @@
-from urllib import request
-
 from django.shortcuts import render
-from .models import Article
+
 from django.http import HttpResponse
 
 
-def year_archive(year=None, *args, **kwargs):
-    a_list = Article.objects.filter(pub_date__year=year)
-    context = {'year': year, 'article_list': a_list}
-    return render(request, 'news/year_archive.html', context)
+def year_archive(request):
+    return render(request, "year_archive.html", {})
 
 # Create your views here.
