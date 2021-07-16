a42fb9b0dfee5e111879c38213e44290916e9a32
SimonGideon <simongideon918@gmail.com>
2021-07-16 14:31:54 +0300

    Walk over

--- Django_1/source/Pages/views.py
+++ Django_1/source/Pages/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def contact_view(request):
+    return HttpResponse("<h1>Contacts Page</h1>")
+
+
+def about_view(request):
+    return HttpResponse("<h1>About Page</h1>")
+
+
+def social_viw(request):
+    return HttpResponse("<h1>Contacts Page</h1>")
--- S_dan/Scr/polls/views.py
+++ S_dan/Scr/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("THis is another trial. FOr my hands on django")
+    return HttpResponse("This is another trial. For my hands on django")
