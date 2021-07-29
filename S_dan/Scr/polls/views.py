b625bdaf63c191c549894575cd047e210dc73387
SimonGideon <simongideon918@gmail.com>
2021-07-29 06:15:40 +0300

    Initial Commit

--- S_dan/Scr/polls/views.py
+++ S_dan/Scr/polls/views.py
@@ -19,3 +19,10 @@ def about(request, *args, **kwargs):
         "my_list": [123, 4675, 376465]
     }
     return render(request, 'about.html', {})
+def details(request, question_id)
+    return HttpResponse("You're looking at the question%s" % question_id)
+def results(request, question_id):
+    response = "You're looking at the results of questions %s."
+    return HttpResponse(response% question_id)
+def vote(request, question_id):
+    return HttpResponse("")
