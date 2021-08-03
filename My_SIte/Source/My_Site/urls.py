a5e1bb941357ae77a90fab6cd453f43a347fbe7b
SimonGideon <simongideon918@gmail.com>
2021-08-03 21:39:03 +0300

    initial commit

--- My_SIte/Source/My_Site/urls.py
+++ My_SIte/Source/My_Site/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('<int:question_id>/', views.details, name='details'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('vote/', views.vote, name='vote'),
     path('admin/', admin.site.urls),
 ]
--- My_SIte/Source/Polls/views.py
+++ My_SIte/Source/Polls/views.py
@@ -5,6 +5,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Choice, Question
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return  HttpResponseRedirect(reverse('Polls:results', args=(question.id,)))
+
 
 
 # Create your views here.
