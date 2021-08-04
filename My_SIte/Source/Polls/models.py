89dd91966ed21beeca38a76061efb58c11ce5fb4
SimonGideon <simongideon918@gmail.com>
2021-08-04 22:19:23 +0300

    bug test

--- My_SIte/Source/Polls/models.py
+++ My_SIte/Source/Polls/models.py
@@ -5,6 +5,11 @@ from django.utils import timezone
 
 
 # Create your models here.
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
 class Question(models.Model):
     def __str__(self):
         return self.question_text
