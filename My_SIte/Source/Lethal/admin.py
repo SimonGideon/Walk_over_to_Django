3d260caf1a9129c4e331b77aef82766403f6adb3
SimonGideon <simongideon918@gmail.com>
2021-08-11 22:01:54 +0300

    Django phase 2

---
+++ My_SIte/Source/Lethal/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Musician
+admin.site.register(Musician)
+
+# Register your models here.
--- My_Site/Source/My_Site/settings.py
+++ My_Site/Source/My_Site/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # own
     'news',
     'Polls',
+    'Lethal'
 ]
 
 MIDDLEWARE = [
