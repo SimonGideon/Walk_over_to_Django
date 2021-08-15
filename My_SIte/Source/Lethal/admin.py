09a6119ee87119d9b90652bd39237c6cf3a02cea
SimonGideon <simongideon918@gmail.com>
2021-08-15 19:38:54 +0300

    meta

--- My_SIte/Source/Lethal/admin.py
+++ My_SIte/Source/Lethal/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Musician, Personel
+from .models import Musician, Person
 admin.site.register(Musician)
-admin.site.register(Personel)
+admin.site.register(Person)
 
 # Register your models here.
--- My_SIte/Source/Lethal/models.py
+++ My_SIte/Source/Lethal/models.py
@@ -8,16 +8,6 @@ class Musician(models.Model):
     instrument = models.CharField(max_length=100)
 
 
-class Personel(models.Model):
-    SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
-    name = models.CharField(max_length=60, null=True)
-    shirt_size = models.CharField
-
-
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -74,3 +64,13 @@ class Membership(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateField()
     invite_reason = models.CharField(max_length=64)
+
+
+class Place(models.Model):
+    name= models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+
+class Restaurant(Place):
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
