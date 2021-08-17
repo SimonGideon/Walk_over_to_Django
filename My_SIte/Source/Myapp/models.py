3d7f339735ceba47c609c933504fae5ed6415758
SimonGideon <simongideon918@gmail.com>
2021-08-17 22:04:30 +0300

    meta

--- My_SIte/Source/Myapp/models.py
+++ My_SIte/Source/Myapp/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
+
     def __str__(self):
         return self.name
+
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
+
     def __str__(self):
         return self.name
+
+
 class Entry(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255)
