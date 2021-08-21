f21b8534d20d50280b6e0ba7c7527b41f30906f9
SimonGideon <simongideon918@gmail.com>
2021-08-21 22:29:30 +0300

    pub

--- VEM/source/Can_v/models.py
+++ VEM/source/Can_v/models.py
@@ -1,5 +1,47 @@
+from django.db import models
+
+
+# Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
 class Entry(models.Model):
-    objects = models.Manager()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField()
+    number_of_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=200)
+    data = models.JSONField(null=True)
+
+
+    def __str__(self):
+    return self.name
+
+class Entry(models.Model):
+    object = models.Manager()
     entries = EntryManager()
-    b = Blog.objects.get(id=1)
-    b.entry_set(manager='entries').all()
+
