3ed600863738ecd2b4dcff2ae68347aa79caa18a
SimonGideon <simongideon918@gmail.com>
2021-08-23 23:13:23 +0300

    managers

--- VEM/source/Can_v/models.py
+++ VEM/source/Can_v/models.py
@@ -41,15 +41,25 @@ class Dog(models.Model):
         return self.name
 
 
-class Entry_(models.Model):
-    object = models.Manager()
-
-
-
 class EntryDetails(models.Model):
     entry = models.OneToOneField(Entry, on_delete=models.CASCADE)
     details = models.TextField()
 
 
-ed = EntryDetails.objects.get(id=2)
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
 
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    author = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pubdate = models.DateField()
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book)
