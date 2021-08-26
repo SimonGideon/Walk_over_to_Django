ea0d0655c989bdebbd982f87f607a12c4e05b680
SimonGideon <simongideon918@gmail.com>
2021-08-26 23:20:12 +0300

    declaring table space for index

--- VEM/source/Can_v/models.py
+++ VEM/source/Can_v/models.py
@@ -4,6 +4,16 @@ import random
 
 
 # Create your models here.
+class TablespaceExample(models.Model):
+    name = models.CharField(max_length=30, db_index=True, db_tablespace="indexes")
+    data = models.CharField(max_length=255, db_index=True)
+    shortcut = models.CharField(max_length=7)
+    edges = models.ManyToManyField(to="self", db_tablespace="indexes")
+
+    class Meta:
+        db_tablespace = "tables"
+        indexes = [models.Index(fields=['shortcut'], db_tablespace='other_indexes')]
+
 
 class AuthRouter:
     route_app_labels = {'auth', 'contenttypes'}
