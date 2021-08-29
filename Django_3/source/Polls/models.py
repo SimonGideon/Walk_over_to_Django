aa107572e7e0fc53addecf5c3a3f5c1e75bc530f
SimonGideon <simongideon918@gmail.com>
2021-08-29 22:19:13 +0300

    comment

--- Django_3/source/Polls/models.py
+++ Django_3/source/Polls/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -12,4 +11,12 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+
 # Create your models here.
+def post_comment(request, new_comment):
+    if request.session.get('has_commented', False):
+        return HttpResponse("You've already commented.")
+    c = comment.Comment(comment=new_comment)
+    c.save()
+    request.session['has_commented'] = True
+    return HttpResponse("Thanks for your comment!")
