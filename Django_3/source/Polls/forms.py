8aa25fae77d6e0db5dd3bf8b859ac3c5eb9ed094
SimonGideon <simongideon918@gmail.com>
2021-08-30 21:17:02 +0300

    forms

--- Django_3/source/Polls/forms.py
+++ Django_3/source/Polls/forms.py
@@ -1,1 +1,9 @@
-<form action="/you-name8"
+from django import forms
+
+
+class Contact(forms.Form):
+    name = forms.CharFields()
+    email = forms.Emailfield(label='E-Mail')
+    category = forms.ChoiceField(choices=('question', 'Question'))
+    subject = forms.CharField(required=False)
+    body = forms.CharField(widget=forms.Textarea)
