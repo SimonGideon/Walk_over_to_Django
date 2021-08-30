25f0d97dd4ad2e15568d90ec3187441e8b2d37b8
SimonGideon <simongideon918@gmail.com>
2021-08-30 21:40:00 +0300

    forms

--- Django_3/source/Polls/views.py
+++ Django_3/source/Polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from forms import Contact
 
 
 # Create your views here.
 def contact(request):
-    return HttpResponse('contact, view')
+    form = contact()
+    return render(request, 'form.html', {'form': form})
