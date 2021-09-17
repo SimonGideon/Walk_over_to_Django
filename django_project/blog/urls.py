d364b36dc07e304951d2d6e29edb8631b4eba20b
SimonGideon <simongideon918@gmail.com>
2021-09-17 22:08:41 +0300

    Log in temp

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from. import views
+from users import views as user_views
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('register/', user_views.register, name='register'),
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from.models import Post
+from .models import Post
+
+
 # Create your views here.
-posts = [
-    {
-        'author': 'Mark',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jone',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 27, 2019'
-    },
-]
-
-
 def home(request):
     context = {
         'posts': Post.objects.all()
