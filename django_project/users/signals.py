369b09dca9fc48bfc3bb331e91c35cc54f3e5bf8
SimonGideon <simongideon918@gmail.com>
2021-09-20 23:34:04 +0300

    Initial Commit

--- django_project/users/signals.py
+++ django_project/users/signals.py
@@ -12,4 +12,4 @@ def create_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    instance.Profile.save()
+    instance.profile.save()
