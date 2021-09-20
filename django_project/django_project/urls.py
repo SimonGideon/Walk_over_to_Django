087734a078958a956d6aae6f209b445500a140bb
SimonGideon <simongideon918@gmail.com>
2021-09-20 22:14:37 +0300

    Initial Commit

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
