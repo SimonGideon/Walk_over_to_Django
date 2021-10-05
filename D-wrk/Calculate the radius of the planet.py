97105c814b7a4a5689a0cde882e7c30f2af67da9
SimonGideon <simongideon918@gmail.com>
2021-10-06 00:20:03 +0300

    Planet width

--- D-wrk/Calculate the radius of the planet.py
+++ D-wrk/Calculate the radius of the planet.py
@@ -1,7 +1,9 @@
 from math import pi
 r = input("Radius of the orbit")
-v = input("Obital Speed")
+v = input("Orbital Speed")
 r = float(r)
 v = float(v)
 
-r = r*pi*r/v
+r = r*100000000
+year = 2 *pi*r/v
+year = year / (60*60*24)
