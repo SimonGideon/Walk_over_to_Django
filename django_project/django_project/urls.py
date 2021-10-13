cb9cdfdb304d8463517aa94d951cf678c38f57a1
SimonGideon <simongideon918@gmail.com>
2021-10-13 06:13:37 +0300

    Password reset

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -25,14 +25,18 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView(template_name='users/password_reset.html'), name= 'password_reset'),
-    path('password-reset/done', auth_views.PasswordResetDOneView(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password-reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'),
+         name='password-reset-confirm'),
 
     path('profile/', user_views.profile, name='profile'),
 
     path('', include('blog.urls')),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
