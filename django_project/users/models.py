86224d6c83fd05fef0847906ea9afca30f7138d7
SimonGideon <simongideon918@gmail.com>
2021-11-09 16:15:50 +0300

    Start Up Html

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -9,14 +9,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+#  def __str__(self):
+#      return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+#  def save(self, *args, **kwargs):
+#     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+#   img = Image.open(self.image.path)
+#    if img.height > 300 or img.width > 300:
+#       output_size = (300, 300)
+#      img.thumbnail(output_size)
+#       img.save(self.image.path)
